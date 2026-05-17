e535fa266f87f27a3ee184a20ea1879b8c321de7
ranakaleem786 <ranakaleem786422@gmail.com>
2026-05-17 15:27:50 +0500

    backend api set

--- src/lib/api.js
+++ src/lib/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const apiClient = axios.create({
-  baseURL: "http://localhost:5000/api/",
+  baseURL: "https://hostmost-api.vercel.app/api/",
   withCredentials: true,
 });
 
